51e2df94fe528e6d8610880e9e37a140c2768304
NatalyaPeshkova <fros.frost2010@yandex.ru>
2022-08-31 14:04:50 +1000

    do function appearAfterDelay

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 
+var timeTakenValue = document.querySelector('.timeTaken');
 var start = new Date().getTime();
+
+function makeShapeAppear() {
+    document.querySelector('.shape').style.display = "block";
+    start = new Date().getTime();
+}
+
+function appearAfterDelay() {
+setTimeout(makeShapeAppear, Math.random() * 2000);
+}
+appearAfterDelay(); 
 
 document.querySelector('.shape').onclick = function () {
     document.querySelector('.shape').style.display = "none";
     var end = new Date().getTime();
     timeTaken = (end - start)/1000;
-    alert("Вам потребовалось" + timeTaken  + " секунд ");
-    
+    timeTakenValue.textContent = timeTaken;
+    appearAfterDelay();
 }
+
